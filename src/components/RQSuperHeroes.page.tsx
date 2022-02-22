76b4575dc900382897f873b96ddad107015e712f
Ronit Panda <pandaronit25@gmail.com>
2022-02-22 20:23:16 +0530

    completed the react-query-tut

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,23 +1,17 @@
 /** @format */
 
 import { AxiosResponse } from 'axios';
-import { FC } from 'react';
+import { FC, MouseEventHandler, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useSuperHeroesData } from '../hooks/useSuperHeroesData';
+import {
+  useSuperHeroesData,
+  useAddSuperHeroData,
+} from '../hooks/useSuperHeroesData';
 import { SuperHero } from '../Types/superHero';
 
-//==CACHING==//
-//cache time is default to 5min updating the cache time to 5sec makes the isLoading true every 5sec if you revisit the UI
-//default stale time is 0sec, so whenever the page is visited a background req is made to keep the cache updated but if you know that your data is not updated in the server so often and it's fine for the user to see the old data let's say for 30s then there wont be any network req in the background for 30sec even you visit the page
-//default for refetchOnMount is true it is the one that we need and same as refetchOnWindoFocus is also set to true
-//==POLLING===//
-//making refetchInterval to 2sec will make network request every 2sec, this can be used in applications where the server data changes very frequently
-//refetchIntervalInBackground set to true will make network requests also when the screen looses focus
-//enabled false just does not make a network reques to fetch the data
-//refetch function can be used to fetch the data when the user wants basically like fetching data on a press of a button
-//OnSuccess and Onerror are basically post performance fucntions you can create sideeffects after a certain operation is done
-//the select func changes the data into a format that we need but the types are still fucked
 const RQSuperHeroes: FC = () => {
+  const [name, setName] = useState('');
+  const [alterEgo, setAlterEgo] = useState('');
   //the type of data you set here is the type of data that gets extracted from useQuery
   const onSuccess = (data: AxiosResponse<any, any>) => {
     console.log('Perform side effect after data fetching', data);
@@ -27,25 +21,59 @@ const RQSuperHeroes: FC = () => {
     console.log('Perform side effect after encountering error', error);
   };
 
-  const { isLoading, data, isError, error, isFetching } = useSuperHeroesData(
+  const { isLoading, data, isError, error, refetch } = useSuperHeroesData(
     onSuccess,
     onError
   );
 
-  if (isLoading) {
+  const {
+    mutate: addHero,
+    isLoading: addIsLoading,
+    isError: addIsError,
+  } = useAddSuperHeroData();
+
+  if (isLoading || addIsLoading) {
     return <h2>Loading....</h2>;
   }
 
-  if (isError) {
+  if (isError || addIsError) {
     return <h2>{(error as any).message}</h2>;
   }
 
-  console.log(isLoading, isFetching);
+  const handleHeroClick = () => {
+    console.log({ name, alterEgo });
+    const hero: SuperHero = {
+      name: name,
+      alterEgo: alterEgo,
+      id: Math.random() * 100 + 6,
+    };
+    addHero(hero);
+  };
 
   return (
     <div>
       <h2>SuperHeroes</h2>
+      <div>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type='text'
+          value={alterEgo}
+          onChange={(e) => {
+            setAlterEgo(e.target.value);
+          }}
+        />
+        <button onClick={handleHeroClick}>Add Hero</button>
+      </div>
       <ul>
+        <button onClick={refetch as MouseEventHandler<HTMLButtonElement>}>
+          Fetch Heroes
+        </button>
         {data?.data.map((hero: SuperHero) => {
           return (
             <li key={hero.id}>
--- src/hooks/useSuperHeroesData.tsx
+++ src/hooks/useSuperHeroesData.tsx
@@ -1,7 +1,8 @@
 /** @format */
 
 import axios, { AxiosResponse } from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { SuperHero } from '../Types/superHero';
 
 const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes');
@@ -14,5 +15,44 @@ export const useSuperHeroesData = (
   return useQuery('super-heroes', fetchSuperHeroes, {
     onSuccess,
     onError,
+  });
+};
+
+const addSuperHero = (hero: SuperHero) => {
+  return axios.post('http://localhost:4000/superheroes', hero);
+};
+
+//optimistic update
+export const useAddSuperHeroData = () => {
+  const qc = useQueryClient();
+  return useMutation(addSuperHero, {
+    // onSuccess: (data) => {
+    //   qc.setQueryData('super-heroes', (oldData: any) => {
+    //     return {
+    //       ...oldData,
+    //       data: [...oldData.data, data.data],
+    //     };
+    //   });
+    //   // qc.invalidateQueries('super-heroes');
+    // },
+    onMutate: async (newHero: SuperHero) => {
+      await qc.cancelQueries('super-heroes');
+      const prevQueryData = qc.getQueryData('supper-heroes');
+      qc.setQueryData('super-heroes', (oldData: any) => {
+        return {
+          ...oldData,
+          data: [...oldData.data, newHero],
+        };
+      });
+      return {
+        prevQueryData,
+      };
+    },
+    onError: (_error, _hero, context) => {
+      qc.setQueryData('super-heroes', context?.prevQueryData);
+    },
+    onSettled: () => {
+      qc.invalidateQueries('super-heroes');
+    },
   });
 };
